37ade1899c3d990b6f0db66c38256a58ad33e04e
takacszsolt2022d-lab <takacs.zsolt.2022@telekimezotur.hu>
2026-09-21 11:42:31 +0200

    harmadikfeladat

---
+++ JatekApp/Karakter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JatekApp
+{
+    internal class Karakter
+    {
+        public string Nev { get; private set; }
+
+        public int eletero { get; private set; }
+
+        public void Sebezodik(int pont)
+        {
+            if (eletero <= 0)
+            {
+                eletero = 0;
+            }
+            
+            
+
+            
+        }
+    }
+}
