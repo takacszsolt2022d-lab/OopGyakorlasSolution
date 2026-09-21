c1bfb901361156339387a97012f731ee82c5e540
takacszsolt2022d-lab <takacs.zsolt.2022@telekimezotur.hu>
2026-09-21 11:34:44 +0200

    masodikfeladat

---
+++ RaktarApp/Program.cs
@@ -0,0 +1,8 @@
+﻿using RaktarApp;
+Termek t = new Termek();
+
+t.Nev = "alma";
+t.Nev = "cica";
+t.Nev = "kutya";
+
+Console.WriteLine(TermekSzamlalo);
---
+++ RaktarApp/Termek.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Text;
+
+namespace RaktarApp
+{
+    internal class Termek
+    {
+        public string Nev { get; set; }
+
+        public int Egysegar { get; private set; }
+
+        public static int TermekSzamlalo { get; set; }
+
+
+        public void konstruktor(string nev,int egysegar)
+        {
+            nev = Nev;
+            egysegar = Egysegar;
+            TermekSzamlalo++;
+        }
+    }
+}
