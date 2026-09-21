0c79a7e758de3c87d13872b9c1e489d4b4acc332
takacszsolt2022d-lab <takacs.zsolt.2022@telekimezotur.hu>
2026-09-21 11:22:58 +0200

    elsofeladat

---
+++ KonyvtarApp/Konyv.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KonyvtarApp
+{
+    internal class Konyv
+    {
+        public string Cim;
+
+        public int Oldalszam { get;
+            set
+            {
+
+                if (Oldalszam < 0)
+                {
+                    Console.WriteLine("A megadott oldalszam nem lehet negatív.");
+                } else
+                {
+                    return;
+                }
+
+            }
+
+
+             }
+        public void konyv(string cim,int oldalszam) 
+        {
+            cim = Cim;
+            oldalszam = Oldalszam;
+        }
+
+       
+    }
+}
---
+++ KonyvtarApp/Program.cs
@@ -0,0 +1,8 @@
+﻿using KonyvtarApp;
+
+Konyv k = new Konyv();
+
+k.Cim = "Egri Csillagok";
+k.Oldalszam = 500;
+
+Console.WriteLine(k.Cim+","+k.Oldalszam );
